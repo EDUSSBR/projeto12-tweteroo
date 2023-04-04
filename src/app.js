26a4d1d09fba7295f0afb4cc5884526b08dd4ecf
Eduardo Souto dos Santos <bls.dudu@gmail.com>
2023-04-04 14:52:00 -0300

    add users and tweet memory repo and setup express for json

--- src/app.js
+++ src/app.js
@@ -2,5 +2,9 @@ import express from "express";
 const app = express();
 const PORT = 5000;
 
+app.use(express.json());
+
+let users = []
+let tweets = []
 
 app.listen(PORT, () => console.log(`Server listening at: ${PORT}`))
